9786691f9f93aaea5cafb38d878fa324bfb5a03c
ruben <localgit@r.z11.de>
2015-11-23 00:15:21 +0100

    changed logging into files

--- build-all-targets.sh
+++ build-all-targets.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-OPTIONS="BROKEN=1 DEFAULT_GLUON_RELEASE=0.8~exp$(date  '+%Y%m%d%H%M')"
+VERSION=0.8~exp$(date  '+%Y%m%d%H%M')
+OPTIONS="BROKEN=1 DEFAULT_GLUON_RELEASE=$VERSION"
 CORES=$(lscpu|grep -e '^CPU(s):'|xargs|cut -d" " -f2)
+LOGBASE=/tmp/gluon-build-log-$VERSION
 set -x
-for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu; do
-  : "################# $TARGET ###########################"
-  make -j$CORES GLUON_TARGET=$TARGET $OPTIONS || break
+for ARCH in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu; do
+  : "################# start building target $ARCH ###########################"
+  make -j$CORES GLUON_TARGET=$ARCH $OPTIONS V=s > $LOGBASE$ARCH 2>&1 || break
   # TODO: gzip successfull build before continuing with next target
 done && : "#### alle Targets wurden erfolgreich erstellt. ###" || : "Fehlschlag! Abbruch"
-
+set +x
+echo "see logfiles in "
+echo $LOGBASE*
+# watch them during build with: tail -f $LOGBASE*
