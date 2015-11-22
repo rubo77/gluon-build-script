d630a8bef39e75d8df8303c9a9a0dea3209b0762
ruben <localgit@r.z11.de>
2015-11-22 23:38:47 +0100

    added Todo: include gzip targets before continuation

--- build-all-targets.sh
+++ build-all-targets.sh
@@ -4,6 +4,7 @@ CORES=$(lscpu|grep -e '^CPU(s):'|xargs|cut -d" " -f2)
 set -x
 for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu; do
   : "################# $TARGET ###########################"
-  echo make -j$CORES GLUON_TARGET=$TARGET $OPTIONS || break 
+  make -j$CORES GLUON_TARGET=$TARGET $OPTIONS || break
+  # TODO: gzip successfull build before continuing with next target
 done && : "#### alle Targets wurden erfolgreich erstellt. ###" || : "Fehlschlag! Abbruch"
 
