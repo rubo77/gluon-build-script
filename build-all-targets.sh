a33e7ed70c64d0b02f9d430bc7fefef17a0ab45c
ruben <localgit@r.z11.de>
2015-11-23 05:41:32 +0100

    v1.0

--- build-all-targets.sh
+++ build-all-targets.sh
@@ -1,15 +1,60 @@
 #!/bin/bash
-VERSION=0.8~exp$(date  '+%Y%m%d%H%M')
+
+# builds all 7 targets for gluon
+#
+# Install:
+#
+# sudo apt-get install git make gcc g++ unzip libncurses5-dev zlib1g-dev subversion gawk bzip2 libssl-dev
+# git clone https://github.com/freifunk-gluon/gluon.git
+# cd gluon
+# # adapt your site/
+# make update
+# ./build-all-targets.sh
+#
+# tip: call this script through ccze: `./build-all-targets.sh | ccze -A `
+
+# Configuration
+
+# version for your build
+VERSION=0.8~exp$(date '+%Y%m%d%H%M')
+
+# Folder to deploy the built images
+ARCHIVE=/var/www/freifunk/firmware/ffki
+
+# logs start and enddate
+LOGFILE=/tmp/gluon-build-log-$VERSION
+
+# Options for make
 OPTIONS="BROKEN=1 DEFAULT_GLUON_RELEASE=$VERSION"
+
+if [[ $EUID -eq 0 ]]; then 
+	echo "cannot be run as root"
+	exit
+fi
+
+# detect amount of CPU cores
 CORES=$(lscpu|grep -e '^CPU(s):'|xargs|cut -d" " -f2)
-LOGBASE=/tmp/gluon-build-log-$VERSION
+
+# ensure archive folder exists for this version
+mkdir -p $ARCHIVE/$VERSION
+
+MESSAGE= "When done, move images into archive with
+rsync -a --info=progress2 output/images/ $ARCHIVE/$VERSION/
+rm -Rf output/images
+
+logfiles: $LOGFILE\*"
+echo $MESSAGE
+
 set -x
-for ARCH in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu; do
-  : "################# start building target $ARCH ###########################"
-  make -j$CORES GLUON_TARGET=$ARCH $OPTIONS V=s > $LOGBASE$ARCH 2>&1 || break
+#for ARCH in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu; do
+for ARCH in ar71xx-generic x86-generic mpc85xx-generic; do
+  trap ": user abort; exit;" SIGINT SIGTERM # so CTRL+C will exit the loop
+  echo "################# $(date) start building target $ARCH ###########################" >> $LOGFILE
+  make -j$CORES GLUON_TARGET=$ARCH $OPTIONS V=s || exit 1
   # TODO: gzip successfull build before continuing with next target
-done && : "#### alle Targets wurden erfolgreich erstellt. ###" || : "Fehlschlag! Abbruch"
+done && : "all targets created in folder output/images/"
 set +x
-echo "see logfiles in "
-echo $LOGBASE*
-# watch them during build with: tail -f $LOGBASE*
+
+echo "################# $(date) finished script ###########################" >> $LOGFILE
+cat $LOGFILE
+echo $MESSAGE
