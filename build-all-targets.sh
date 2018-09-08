481842e6455e16a275a72a70da18a4501ffbf79d
Ruben Barkow <rubo77@users.noreply.github.com>
2018-09-08 19:45:06 +0200

    update to gluon 2018.2.0

--- build-all-targets.sh
+++ build-all-targets.sh
@@ -16,7 +16,7 @@
 # Configuration
 
 # version for your build
-VERSION=2016.5.1~exp$(date '+%Y%m%d%H%M')
+VERSION=2018.2.0~exp$(date '+%Y%m%d%H%M')
 
 # Folder to deploy the built images
 ARCHIVE=/var/www/freifunk/firmware/ffki
@@ -24,12 +24,19 @@ ARCHIVE=/var/www/freifunk/firmware/ffki
 # logs start and enddate
 LOGFILE=/tmp/gluon-build-log-$VERSION
 
+BUILD_BROKEN="BROKEN=1"
+# or BUILD_BROKEN=""
+T="ar71xx-generic ar71xx-tiny ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 mpc85xx-generic ramips-mt7621 sunxi-cortexa7 x86-generic x86-geode x86-64 ipq40xx ramips-mt7620 ramips-mt76x8 ramips-rt305x"
+if [ $BROKEN -ne "" ] ; then
+  T="$T ar71xx-mikrotik brcm2708-bcm2710 ipq806x mvebu-cortexa9"
+fi
+
 # Options for make
-OPTIONS="BROKEN=1 DEFAULT_GLUON_RELEASE=$VERSION"
+OPTIONS="$BUILD_BROKEN DEFAULT_GLUON_RELEASE=$VERSION"
 
 if [[ $EUID -eq 0 ]]; then 
-	echo "cannot be run as root"
-	exit
+  echo "cannot be run as root"
+  exit
 fi
 
 # detect amount of CPU cores
@@ -49,11 +56,11 @@ echo $MESSAGE
 start_timestamp=$(date +%s)
 i=0
 set -x
-for ARCH in ar71xx-generic x86-generic mpc85xx-generic ar71xx-nand x86-kvm_guest x86-64 x86-xen_domu; do
+for TARGET in $T; do
   start[$i]=$(date +%s)
   trap ": user abort; exit;" SIGINT SIGTERM # so CTRL+C will exit the loop
-  echo "################# $(date) start building target $ARCH ###########################" >> $LOGFILE
-  make -j$CORES GLUON_TARGET=$ARCH $OPTIONS V=s || exit 1
+  echo "################# $(date) start building target $TARGET ###########################" >> $LOGFILE
+  make -j$CORES GLUON_TARGET=$TARGET $OPTIONS V=s || exit 1
   # TODO: gzip successfull build before continuing with next target
   echo "Zeit seit Start: "$((($(date +%s)-$start_timestamp)/60))":"$((($(date +%s)-$start_timestamp)%60))" Minuten"
   M="Zeit $TARGET: "$((($(date +%s)-$start[$i])/60))":"$((($(date +%s)-$start[$i])%60))" Minuten"
@@ -66,4 +73,3 @@ echo -n "finished: "; date
 echo "################# $(date) finished script ###########################" >> $LOGFILE
 cat $LOGFILE
 echo $MESSAGE
-
